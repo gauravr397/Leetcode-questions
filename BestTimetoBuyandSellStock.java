2b1cd2e96aaf3cbf72a26aac283ca6e83ea1e7fe
LazyPanda397 <gauravr397@gmail.com>
2022-06-05 23:28:46 +0530

    121. Best Time to Buy and Sell Stock

---
+++ BestTimetoBuyandSellStock.java
@@ -0,0 +1,17 @@
+public class BestTimetoBuyandSellStock {
+    public static void main(String[] args) {
+        BestTimetoBuyandSellStock bs = new BestTimetoBuyandSellStock();
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
+        bs.maxProfit(prices);
+    }
+
+    public int maxProfit(int[] prices) {
+        int sell = 0;
+        int buy = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            buy = Math.min(buy, prices[i]);
+            sell = Math.max(sell, prices[i] - buy);
+        }
+        return sell;
+    }
+}
