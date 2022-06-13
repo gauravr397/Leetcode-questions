8330100768a7a64668327445ae6ac5cb004c85d9
LazyPanda397 <gauravr397@gmail.com>
2022-06-13 18:49:54 +0530

    120. Triangle

---
+++ Triangle.java
@@ -0,0 +1,11 @@
+public class Triangle {
+    public static void main(String[] args) {
+        List<List<Integer>> triangle = {{2},{3,4},{6,5,7},{4,1,8,3}};
+        Triangle t = new Triangle();
+        System.out.println(t.);
+    }
+
+    public int minimumTotal(List<List<Integer>> triangle){
+        int n =
+    }
+}
