fe1cc07a4cc81d3559f552a1d0497a13219b2d58
LazyPanda397 <gauravr397@gmail.com>
2022-06-14 22:51:01 +0530

    509. Fibonacci Number

--- FibonacciNumber.java
+++ FibonacciNumber.java
@@ -6,11 +6,22 @@ public class FibonacciNumber {
     }
 
     public int fib(int n) {
-        if (n <= 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
-        } else
-            return fib(n - 1) + fib(n - 2);
+        // if (n <= 0) {
+        // return 0;
+        // } else if (n == 1) {
+        // return 1;
+        // } else
+        // return fib(n - 1) + fib(n - 2);
+
+        // optimal
+        int ans = 0;
+        int fibo[] = new int[n + 1];
+        fibo[0] = 0;
+        fibo[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+            ans = fibo[i];
+        }
+        return ans;
     }
 }
