47d4cf41f82ff11463fc3577d3e670c73822d90c
LazyPanda397 <gauravr397@gmail.com>
2022-06-17 10:09:45 +0530

    122. Best Time to Buy and Sell Stock II

--- BestTimetoBuyandSellStockII.java
+++ BestTimetoBuyandSellStockII.java
@@ -1,7 +1,7 @@
 public class BestTimetoBuyandSellStockII {
     public static void main(String[] args) {
         int prices[] = { 7, 1, 5, 3, 6, 4 };
-        BestTimetoBuyandSellStock bstbs = new BestTimetoBuyandSellStock();
+        BestTimetoBuyandSellStockII bstbs = new BestTimetoBuyandSellStockII();
         System.out.println(bstbs.maxProfit(prices));
     }
 
