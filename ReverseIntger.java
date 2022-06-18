27ecc90f1fe734083c3856baa73d325828c65e81
LazyPanda397 <gauravr397@gmail.com>
2022-06-18 22:49:17 +0530

    7. Reverse Integer

---
+++ ReverseIntger.java
@@ -0,0 +1,22 @@
+public class ReverseIntger {
+    public static void main(String[] args) {
+        int x = -123;
+        ReverseInteger r = new ReverseInteger();
+        System.out.println(r.reverse(x));
+    }
+
+    public int reverse(int x) {
+        long res = 0;
+        while (x != 0) {
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        if (res < Integer.MIN_VALUE || res > Integer.MAX_VALUE) {
+            return 0;
+
+        } else {
+            return (int) res;
+        }
+    }
+
+}
