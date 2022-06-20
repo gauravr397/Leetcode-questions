ce67f9798487dd3d9e19d4e94503125b0f5d9262
LazyPanda397 <gauravr397@gmail.com>
2022-06-21 00:16:14 +0530

    120. Triangle

--- Triangle.java
+++ Triangle.java
@@ -4,24 +4,6 @@ import java.util.*;
 
 public class Triangle {
     // { { 2 }, { 3, 4 }, { 6, 5, 7 }, { 4, 1, 8, 3 } }
-    // List<List<Integer>> triangle = new ArrayList<List<Integer>>();
-    // List<Integer> list0 = new ArrayList<Integer>();
-    // list0.add(2);
-    // List list1=new ArrayList();
-    // list1.add("List1_Str1");
-    // list1.add("List1_Str2");
-
-    // List list2=new ArrayList();
-    // list2.add("List2_Str1");
-    // list2.add("List2_Str2");
-    // list2.add("List2_Str3");
-
-    // // Create third list
-    // List list3=new ArrayList();
-    // list3.add("List3_Str1");
-    // list3.add("List3_Str2");
-    // list3.add("List3_Str3");
-    // list3.add("List3_Str4");
 
     Triangle t = new Triangle();System.out.println(t.minimumTotal(triangle));
     }
