d58051c17e27f4bc176691b945e7847bb49ce96f
LazyPanda397 <gauravr397@gmail.com>
2022-06-21 00:00:59 +0530

    820. Short Encoding of Words

--- ShortEncodingofWords.java
+++ ShortEncodingofWords.java
@@ -1,15 +1,32 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ShortEncodingofWords {
     public int minimumLengthEncoding(String[] words) {
-        Arrays.sort(words, (a, b) -> b.length() - a.length());
-        StringBuilder sb = new StringBuilder();
+        // // Arrays.sort(words, (a, b) -> b.length() - a.length());
+        // // StringBuilder sb = new StringBuilder();
+        // // for (String s : words) {
+        // // if (sb.indexOf(s + "#") == -1) {
+        // // sb.append(s + "#");
+        // // }
+        // // }
+        // // return sb.length();
+
+        // optimal soln
+
+        Set<String> wordset = new HashSet<>(Arrays.asList(words));
         for (String s : words) {
-            if (sb.indexOf(s + "#") == -1) {
-                sb.append(s + "#");
+            for (int i = 1; i < s.length(); i++) {
+                wordset.remove(s.substring(i));
             }
+
         }
-        return sb.length();
+        int length = 0;
+        for (String s : wordset) {
+            length += s.length() + 1;
+        }
+        return length;
     }
 
 }
