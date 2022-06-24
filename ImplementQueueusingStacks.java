350b829323687f54e9fcd45f6cd887eb357789a8
LazyPanda397 <gauravr397@gmail.com>
2022-06-24 18:06:35 +0530

    232. Implement Queue using Stacks

---
+++ ImplementQueueusingStacks.java
@@ -0,0 +1,37 @@
+public class ImplementQueueusingStacks {
+    Stack<Integer> s1 = new Stack<Integer>();
+    Stack<Integer> s2 = new Stack<Integer>();
+
+    public MyQueue() {
+        
+    }
+
+    public void push(int x) {
+        s1.push(x);
+    }
+
+    public int pop() {
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
+
+    }
+
+    public int peek() {
+        if (!s2.isEmpty()) {
+            return s2.peek();
+        } else {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.peek();
+    }
+
+    public boolean empty() {
+        return s1.isEmpty() && s2.isEmpty();
+    }
+}
