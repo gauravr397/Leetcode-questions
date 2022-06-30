8e695587c5e70f006e3cba5ee275293e013954c8
LazyPanda397 <gauravr397@gmail.com>
2022-07-01 02:16:07 +0530

    136. Single Number

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,5 +1,16 @@
 public class SingleNumber {
     // 136. Single Number
+    /*
+     * nums = 4,1,2,1,2
+     * first XOR will convert the numbers to there bit representation and then
+     * compare those bit with XOR operation
+     * res = 0 xor 4 -> 000 ^ 100 = 100 which is bit value of 4
+     * res = 4 ^ 1 -> 100 ^ 001 = 101 which is bit value of 5
+     * res = 5 ^ 2 -> 101 ^ 010 = 111 which bit value of 7
+     * res - 7 ^ 1 -> 111 ^ 001 = 110 which is bit value of 6
+     * res - 6 ^ 2 -> 110 ^ 010 = 100 which is bit value of 4
+     * and we successfully return non repeating value
+     */
     public static void main(String[] args) {
         SingleNumber s = new SingleNumber();
         int nums[] = { 4, 1, 2, 1, 2 };
