1f0cde1717b02870437c01ced247165f7db906ce
Gaurav Raghav / <gauravr397@gmail.com>
2022-07-02 00:40:02 +0530

    Time: 2 ms (87.17%), Space: 56.1 MB (51.27%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        //Moores voting algo
+       int count=1 , ansindex=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[ansindex]){
+                count++;
+            }
+            else{
+                count--;
+            }
+            if(count==0){
+                ansindex= i;
+                count=1;
+            }
+        }
+        return nums[ansindex];
+    }
+}
