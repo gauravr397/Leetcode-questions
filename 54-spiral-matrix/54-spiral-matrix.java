460b0f1eee0312109f7739b291a14ecb327dff86
Gaurav Raghav / <gauravr397@gmail.com>
2022-07-03 23:03:28 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (87.04%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -6,20 +6,24 @@ class Solution {
         int sc=0;
         int ec=matrix[0].length-1;
         while(sr<=er && sc<=ec){
+            //move right
             for(int i=sc;i<=ec;i++){
                 ans.add(matrix[sr][i]);
             }
             sr++;
+             //move down
             for(int i=sr;i<=er;i++){
                 ans.add(matrix[i][ec]);
             }
             ec--;
+             //move left
             if(sr<=er){
                 for(int i=ec;i>=sc;i--){
                     ans.add(matrix[er][i]);
                 }
             }
             er--;
+             //move top
             if(sc<=ec){
                 for(int i=er;i>=sr;i--){
                     ans.add(matrix[i][sc]);
