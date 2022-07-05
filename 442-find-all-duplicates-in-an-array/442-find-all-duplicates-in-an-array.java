7749bff09db7b6df7e4ffa94e4a537d1a10088c1
Gaurav Raghav / <gauravr397@gmail.com>
2022-07-05 22:16:53 +0530

    Time: 12 ms (38.91%), Space: 63.6 MB (78.97%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
@@ -1,27 +1,24 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        // List<Integer> ans = new ArrayList<>();
-        // Set<Integer> set= new HashSet<>();
-        // if(nums.length<=1)return null;
-        // for(int i=0;i<nums.length;i++){
-        //     if(!set.contains(nums[i]))
-        //         set.add(nums[i]);
-        //     else
-        //         ans.add(set.get(nums[i]));
-        // }
-        // return ans;
+       
+//         List<Integer> res = new ArrayList<>();
+//         for(int i=0;i<nums.length;i++){
+//             int index=Math.abs(nums[i])-1;
+//             if(nums[index]<0)
+//                 res.add(Math.abs(nums[index]));
+            
+//             nums[index] *= -1;
+//         }
+//         return res;
         
         
-        List<Integer> res = new ArrayList<>();
-        int i=0;
-        Arrays.sort(nums);
-        while(i<(nums.length-1)){
-            if(nums[i] == nums[i+1]){
-                res.add(nums[i]);
-                
+        List<Integer>res=new ArrayList<>();
+        for(int i=0;i<nums.length;++i){
+            int index=Math.abs(nums[i])-1;
+            if(nums[index]<0)
+                res.add(Math.abs(nums[i]));
+            nums[index]=-nums[index];
             }
-            i++;
-        }
         return res;
     }
 }
