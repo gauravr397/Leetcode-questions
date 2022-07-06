617369e791e9518b3823e214ae736318b3a125ec
Gaurav Raghav / <gauravr397@gmail.com>
2022-07-07 00:27:32 +0530

    Time: 8 ms (27.49%), Space: 49.1 MB (7.13%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.java
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.java
@@ -10,10 +10,8 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-         // Stack<ListNode> s1=new Stack<>();
-         // Stack<ListNode> s2=new Stack<>(); 
-        Deque<ListNode> s1 = new ArrayDeque<>();
-    Deque<ListNode> s2 = new ArrayDeque<>();
+         Stack<ListNode> s1=new Stack<>();
+         Stack<ListNode> s2=new Stack<>(); 
         while(l1!=null){
             s1.push(l1);
             l1=l1.next;
