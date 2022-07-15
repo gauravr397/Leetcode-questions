af1494b064264c6744959ad59e63cf840eff6cdf
Gaurav Raghav / <gauravr397@gmail.com>
2022-07-15 12:15:34 +0530

    Time: 2 ms (89.66%), Space: 53.8 MB (76.02%) - LeetHub

---
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minMoves(int[] nums) {
+        int count=0;
+        int min=nums[0];
+        for(int i:nums){
+            min=Math.min(min,i);
+        }
+        for(int i:nums){
+            count+=Math.abs(min-i);
+        }
+        return count;
+    }
+}
