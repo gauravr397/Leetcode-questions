f4b1cb07da0b156e56c030af62893c2f544156bd
Gaurav Raghav / <gauravr397@gmail.com>
2022-07-15 13:01:43 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (97.85%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,12 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n=nums.length;
-        int nsum=(n*(n+1))/2;
+        int n =nums.length;
+        int nsum = (n*(n+1))/2;
         int tot=0;
         for(int i:nums){
-            tot+= i;
+            tot+=i;
         }
-        
         return nsum-tot;
     }
 }
