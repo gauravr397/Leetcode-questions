842d849aac513a51d0d99d9378a14b34211e26b3
Gaurav Raghav / <gauravr397@gmail.com>
2022-07-19 01:04:54 +0530

    Time: 44 ms (47.89%), Space: 68.5 MB (13.51%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -5,8 +5,8 @@ class Solution {
         map.put(0,1);
         for(int i:nums){
             sum+=i;
-            if(map.containsKey(sum-k)){
-                ans+=map.get(sum-k);
+            if(map.containsKey(sum-k)){ //gin mil gya in map 
+                ans+=map.get(sum-k); // inc ans with freq
             }
             
             map.put(sum,map.getOrDefault(sum,0)+1);
