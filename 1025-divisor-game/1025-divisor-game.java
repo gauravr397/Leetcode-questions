734477bd3bd1cbb8cdf7f814f2837305dc000bf3
Gaurav Raghav / <gauravr397@gmail.com>
2022-07-20 00:01:32 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (50.60%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,8 +1,10 @@
 class Solution {
     public boolean divisorGame(int n) {
-        if(n%2==0)
-            return true;
-        else
-            return false;
+//         if(n%2==0)
+//             return true;
+//         else
+//             return false;
+        
+        return n%2==0;
     }
 }
