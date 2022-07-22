45ad7e10a2cd4222f18db71aee07ab920472ef61
Gaurav Raghav / <gauravr397@gmail.com>
2022-07-22 18:57:09 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (36.60%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -20,19 +20,19 @@ class Solution {
         //Reverse Approach
         
         int n=matrix.length;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                int temp = matrix[i][j];
-                matrix[i][j]=matrix[j][i];
-                matrix[j][i]=temp;
+        for(int i=0;i<n;i++){                   // -
+            for(int j=i;j<n;j++){                // -
+                int temp = matrix[i][j];        //   --- make transopose of matrix
+                matrix[i][j]=matrix[j][i];       // - 
+                matrix[j][i]=temp;              // -
             }
         }
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n/2;j++){
-                int temp = matrix[i][j];
-                matrix[i][j]=matrix[i][n-1-j];
-                matrix[i][n-1-j]=temp;
+        for(int i=0;i<n;i++){                    // -
+            for(int j=0;j<n/2;j++){              //  -
+                int temp = matrix[i][j];         //   --- swap 1st and last val in each row
+                matrix[i][j]=matrix[i][n-1-j];   //  -
+                matrix[i][n-1-j]=temp;           // -
                 
             }
             
