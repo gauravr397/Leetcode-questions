876a9643e750fc4e638a117859187dde34df0ef6
Gaurav Raghav / <gauravr397@gmail.com>
2022-07-22 18:47:03 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (91.23%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -21,7 +21,7 @@ class Solution {
         
         int n=matrix.length;
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
+            for(int j=i;j<n;j++){
                 int temp = matrix[i][j];
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=temp;
