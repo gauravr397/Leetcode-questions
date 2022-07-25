4c3a31b091c4353d5ff0fde0e21eae3c069cda74
Gaurav Raghav / <gauravr397@gmail.com>
2022-07-25 22:56:30 +0530

    Time: 5 ms (38.00%), Space: 67.4 MB (70.25%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int i=0;
-        
-        for(int reach=0;i<nums.length && i<= reach;i++){
+        int reach =0;
+        while(i<nums.length && i<= reach){
             reach = Math.max(reach,i+nums[i]);
+            i++;
         }
         return i==nums.length;
        
