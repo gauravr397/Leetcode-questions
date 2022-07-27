4f53a3dad932787bfcfed182d2b5c0ecacbd3f16
Gaurav Raghav / <gauravr397@gmail.com>
2022-07-27 15:21:13 +0530

    Time: 654 ms (17.22%), Space: 152.3 MB (20.35%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -19,5 +19,35 @@ class Solution {
         return ans;
         
         
+        
+        //OR  TLE
+        
+//         Set<Integer> set = new HashSet();
+        
+//        int max = -1;
+//         int min = 1000000000;
+//         for(int i:nums){
+//             set.add(i);
+//             min = Math.min(min,i);
+//             max = Math.max(max,i);
+//         }
+        
+//         int count =0;
+//         int res=0;
+//         for(int i=min;i<=max;i++){
+//             if(set.contains(i)){
+//                 count++;
+//                 set.remove(i);
+                
+//             }
+//             else{
+//                 count=0;
+//             }
+//             res= Math.max(res,count);
+//             if(set.size()<=res && count==0)break;
+//         }
+//         return res;
+        
+        
     }
 }
