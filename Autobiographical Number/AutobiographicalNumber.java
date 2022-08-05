7cb4053f1db54364005e8a4813f6a07c0de6e296
LazyPanda397 <gauravr397@gmail.com>
2022-08-05 17:32:07 +0530

    Autobiographical Number

---
+++ Autobiographical Number/AutobiographicalNumber.java
@@ -0,0 +1,13 @@
+/*
+    
+ */
+
+import java.util.*;
+
+public class AutobiographicalNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+}
