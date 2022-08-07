a2b86734cd250f758268b4177bdf6dc587193779
Gaurav Raghav / <gauravr397@gmail.com>
2022-08-07 22:47:38 +0530

    Time: 2 ms (68.46%), Space: 41.5 MB (32.87%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,19 +1,17 @@
 class Solution {
     public boolean isHappy(int n) {
-        Set<Integer> set= new HashSet<>();
+     Set<Integer> set = new HashSet();
         while(n!=1){
-            if(set.contains(n)){
+            if(set.contains(n))
                 return false;
-            }
             set.add(n);
             int sum=0;
             while(n>0){
-                sum += (n%10) * (n%10);
+                sum+=(n%10)*(n%10);
                 n/=10;
             }
             n=sum;
         }
         return true;
-        
     }
 }
