9e39da33e724c6d64adac9c1ed50161ee8cf4c87
Gaurav Raghav / <gauravr397@gmail.com>
2022-08-08 22:15:56 +0530

    Time: 7 ms (70.78%), Space: 42.3 MB (87.70%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.java
+++ 43-multiply-strings/43-multiply-strings.java
@@ -7,9 +7,9 @@ class Solution {
         if("1"==num2)return num1;
         
         int res[]=new int[m+n];
-        for(int i=m-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                int product=(num1.charAt(j)-'0') * (num2.charAt(i)-'0');
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int product=(num1.charAt(i)-'0') * (num2.charAt(j)-'0');
                 product+=res[i+j+1];
                 res[i+j+1]=product%10;
                 res[i+j]+=product/10;
