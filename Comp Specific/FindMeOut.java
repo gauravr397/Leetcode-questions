9168c4772238f07ccf056fc68ea9100aa52f81ae
LazyPanda397 <gauravr397@gmail.com>
2022-08-29 18:48:27 +0530

    FIndMeOut

---
+++ Comp Specific/FindMeOut.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class FindMeOut {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int res = 0;
+        int n = sc.nextInt();
+        for (int i = n + 1; i < 1000; i++) {
+            if (getsum(i) == n && i % n == 0) {
+                res = i;
+                break;
+            }
+        }
+        System.out.println(res);
+    }
+
+    static int getsum(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+}
