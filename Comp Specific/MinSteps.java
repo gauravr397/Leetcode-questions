409c497ab75cd005c5d915d97717fe8e890c5744
LazyPanda397 <gauravr397@gmail.com>
2022-08-30 11:32:27 +0530

    MinSteps

---
+++ Comp Specific/MinSteps.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class MinSteps {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        // int a = 1, b = 1;
+        int count = 0;
+        System.out.println(solve(x, y, count));
+    }
+
+    public static int solve(int x, int y, int count) {
+        // int count = 0;
+        if (x == 1 && y == 1) {
+            return count;
+        }
+        if (x == y || x < 1 || y < 1) {
+            return -1;
+        }
+        if (x > y) {
+            count++;
+            return solve(x - y, y, count);
+        } else {
+            count++;
+            return solve(x, y - x, count);
+        }
+
+    }
+}
