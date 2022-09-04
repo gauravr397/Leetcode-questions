40fe35b4a182f23836fcd623a8cf37361b79c614
LazyPanda397 <gauravr397@gmail.com>
2022-09-04 16:08:15 +0530

    SecretCode

---
+++ Comp Specific/Secretcode.java
@@ -0,0 +1,18 @@
+
+/*Question :
+    You need To Generate a secret code.
+    Input:
+    You are given a , you will have to change the digits of the number to
+    generate secret code .
+    If digit is positive , then increase by 1.
+    If digit is negative , then decrease by 1.
+    sample input:
+    45671
+    Output
+    54760
+*/
+import java.util.*;
+
+public class Secretcode {
+
+}
